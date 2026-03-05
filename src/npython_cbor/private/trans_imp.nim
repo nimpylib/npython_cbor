95d5860ab1c6be56de642a7089f490b923582d4b
litlighilit <litlighilit@foxmail.com>
2026-03-05 19:12:07 +0800

    chore(dep): trans_imp (from npython, handy_sugars)

--- src/npython_cbor/private/trans_imp.nim
+++ src/npython_cbor/private/trans_imp.nim
@@ -1,14 +1,4 @@
 
-import std/macros
-proc dotSlash(id: NimNode): NimNode = prefix(id, "./")
-macro impExp*(pre, names) =
-  result = newStmtList()
-  let imp = infix(pre, "/", names)
-  result.add nnkImportStmt.newTree(imp)
-  let exp = newNimNode nnkExportStmt
-  for name in names:
-    exp.add name
-  result.add exp
-macro impExpCwd*(pre, names) =
-  getAst impExp(dotSlash(pre), names)
+import pkg/Utils/trans_imp
+export trans_imp
 
