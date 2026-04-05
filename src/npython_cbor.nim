10a36a6c628b1b1b73b80b7ca809deda9d51e2b3
litlighilit <litlighilit@foxmail.com>
2026-04-05 08:17:31 +0800

    feat(typst): wip

--- src/npython_cbor.nim
+++ src/npython_cbor.nim
@@ -6,6 +6,16 @@ impExpCwd npython_cbor, [impl, cborLib]
 
 when isMainModule:
   import ./npython_cbor/[npy_main_module]
-  Py_Initialize()
-  cbor_Init()
-  main(init=false)
+  template pyinit =
+    Py_Initialize()
+    cbor_Init()
+  when defined(typst):
+    import pkg/wasm_minimal_protocol
+    gen_wasm_init pyinit()
+
+    proc PyRun_String*(str: string; globals: PyDictObject): PyObject{.export_typst_conv(ncTypst).} =
+      PyRun_String(str, Eval, globals, globals)
+    genTypstFile()
+  else:
+    pyinit()
+    main(init=false)
